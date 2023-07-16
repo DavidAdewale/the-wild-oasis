a73e9e3be6348516a5c7750502fb7d5131e39da4
unknown <adewaledavidola@gmail.com>
2023-07-16 17:23:25 +0100

    added some styled components

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,31 @@
+import { styled } from 'styled-components';
+import GlobalStyles from './styles/GlobalStyles';
+import Input from './ui/Input';
+import Button from './ui/Button';
+
+const H1 = styled.h1`
+  font-size: 30px;
+  font-weight: 600;
+  background-color: yellow;
+`;
+
+const StyledApp = styled.div`
+  background-color: orangered;
+  padding: 20px;
+`;
+
 function App() {
-  return <div>Hello world</div>;
+  return (
+    <>
+      <GlobalStyles />
+      <StyledApp>
+        <H1>The Wild Oasis</H1>
+        <Button onClick={() => alert('Check in')}>Check in</Button>
+        <Button onClick={() => alert('Check out')}>Check out</Button>
+        <Input type="number" placeholder="number of guests" />
+      </StyledApp>
+    </>
+  );
 }
 
 export default App;
